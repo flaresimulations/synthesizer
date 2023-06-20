4e59b429c6b5a53217d22b0ba30e2f54d2e67917
Stephen Wilkins <sw376@mps026505.mps.schools.susx.ac.uk>
2023-06-20 14:26:56 +0100

    largely updated cloudy input grid generator

--- generate_grids/sps/cloudy/make_cloudy_input_grid.py
+++ generate_grids/sps/cloudy/make_cloudy_input_grid.py
@@ -2,6 +2,7 @@
 Create a grid of cloudy models
 """
 
+import numpy as np
 import argparse
 from pathlib import Path
 import yaml
@@ -10,345 +11,176 @@ from synthesizer.abundances import Abundances
 from synthesizer.grid import Grid
 from synthesizer.cloudy import create_cloudy_input, ShapeCommands
 
-from write_submission_script import (apollo_submission_script,
-                                     cosma7_submission_script)
+from utils import apollo_submission_script, get_grid_properties
 
 from copy import deepcopy
 
 
-def load_cloudy_parameters(param_file='default.yaml', default_param_file='default.yaml',
-                           **kwarg_parameters):
+def load_grid_params(param_file='c17.03', dir = 'params'):
     """
-    Load CLOUDY parameters from a YAML file
+    parameters from a single param_file
 
     Parameters
     ----------
     param_file : str
         Location of YAML file.
-    **kwarg_parameters
-        Additional parameters you may wish to amend manually
 
     Returns
     -------
-    bool
-        True if successful, False otherwise.
+    dict
+        Dictionary of cloudy parameters
     """
-
-    cloudy_params = {}
 
     # open paramter file
-    with open(param_file, "r") as stream:
+    with open(f'{dir}/{param_file}.yaml', "r") as stream:
         try:
-            cloudy_params_ = yaml.safe_load(stream)
+            params = yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
 
-    # open default parameter file
-    with open(default_param_file, "r") as stream:
-        try:
-            default_cloudy_params = yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
+    grid_params = {}
+    fixed_params = {}
 
-    out_str_ = ''
-
-    # flag denoting that the parameter file includes at least one list. Used below.
-    parameter_list = False
-    for k, v in cloudy_params_.items():
-        if type(v) is not list:
-            if v != default_cloudy_params[k]:
-                out_str_ += f'-{k}{str(v).replace("-", "m")}'
-                cloudy_params[k] = v
+    for k, v in params.items():
+        if isinstance(v, list):
+            grid_params[k] = v
         else:
-            parameter_list = True
+            fixed_params[k] = v
 
-    # copy over defaults where the parameter is not already set
-    for k, v in default_cloudy_params.items():
-        if k not in cloudy_params.keys():
-            cloudy_params[k] = v
-
-    # first loop through and identify changes that are not in lists (e.g. U_model) and add them to the model name
-    out_str_ = f"cloudy-{cloudy_params['cloudy_version']}"+out_str_
-
-    # search for any lists of parameters.
-    # currently exits once it finds the *first* list
-    # TODO: adapt to accept multiple varied parameters
-
-    if not parameter_list:
-
-        output_cloudy_params = [cloudy_params]
-        output_cloudy_names = [out_str_]
-
-    else:
-
-        output_cloudy_params = []
-        output_cloudy_names = []
-
-        for k, v in cloudy_params_.items():
-            if type(v) is list:
-                for _v in v:
-
-                    cloudy_params_var = deepcopy(cloudy_params)
-
-                    # update the value in our default dictionary
-                    cloudy_params_var[k] = _v
-
-                    # save to list of cloudy param dicts
-                    output_cloudy_params.append(cloudy_params_var)
-
-                    # replace negative '-' with m
-                    # out_str = f'-{k}{str(_v).replace("-", "m")}'
-                    out_str = f'-{k}{str(_v).replace("-", "m")}'
-
-                    # save to list of output strings
-                    output_cloudy_names.append(out_str_+out_str)
-
-    return output_cloudy_params, output_cloudy_names
+    return fixed_params, grid_params
 
 
-# def load_cloudy_parameters(param_file='default.yaml', default_param_file='default.yaml',
-#                            **kwarg_parameters):
-#     """
-#     Load CLOUDY parameters from a YAML file
-#
-#     Parameters
-#     ----------
-#     param_file : str
-#         Location of YAML file.
-#     **kwarg_parameters
-#         Additional parameters you may wish to amend manually
-#
-#     Returns
-#     -------
-#     bool
-#         True if successful, False otherwise.
-#     """
-#
-#     # open paramter file
-#     with open(param_file, "r") as stream:
-#         try:
-#             cloudy_params = yaml.safe_load(stream)
-#         except yaml.YAMLError as exc:
-#             print(exc)
-#
-#     # open default parameter file
-#     with open(default_param_file, "r") as stream:
-#         try:
-#             default_cloudy_params = yaml.safe_load(stream)
-#         except yaml.YAMLError as exc:
-#             print(exc)
-#
-#     # find any differences - this breaks when a list is defined
-#     cloudy_params_set = set(cloudy_params.items())
-#     default_cloudy_params_set = set(default_cloudy_params.items())
-#
-#     param_changes = dict(cloudy_params_set - default_cloudy_params_set)
-#
-#     # update any custom parameters
-#     for k, v in kwarg_parameters.items():
-#         cloudy_params[k] = v
-#         param_changes[k] = v
-#
-#     out_str = ''
-#     for k, v in param_changes.items():
-#         out_str += f'-{k}{str(v).replace("-", "m")}'
-#
-#     return [cloudy_params], [out_str]
 
 
-def make_directories(synthesizer_data_dir, sps_grid, cloudy_name):
-    """
-    Create required directories for storing CLOUDY input and output files
 
-    Parameters
-    ----------
-    synthesizer_data_dir : str
-        the top level output directory.
-    sps_grid : str
-        The name of the SPS grid.
-    cloudy_name : str
-        The cloudy specific post-fix for the output directory name
 
-    Returns
-    -------
-    output_dir : str
-        Name of the output directory.
-    """
+if __name__ == "__main__":
 
-    output_dir = f'{synthesizer_data_dir}/cloudy/{sps_grid}_{cloudy_name}'
+    parser = argparse.ArgumentParser(description='Run a grid of SPS cloudy models')
+    parser.add_argument("-machine", type=str, required=True) # machine (for submission script generation)
+    parser.add_argument("-synthesizer_data_dir", type=str, required=True) # path to synthesizer_data_dir
+    parser.add_argument("-sps_grid", type=str, required=True) # grid_name, used to define parameter file
+    parser.add_argument("-cloudy_grid", type=str, required=False, default='params/default.yaml') # the parameters of the cloudy run, including any grid axes
+    parser.add_argument("-cloudy_path", type=str, required=True) # path to cloudy directory (not executable; this is assumed to {cloudy}/{cloudy_version}/source/cloudy.ext)
+    args = parser.parse_args()
 
+
+    # load the cloudy parameters you are going to run
+    fixed_params, grid_params = load_grid_params(args.cloudy_grid)
+
+
+    # open the parent sps grid
+    sps_grid = Grid(args.sps_grid, grid_dir=f'{args.synthesizer_data_dir}/grids')
+
+    # define output directories 
+    output_dir = f'{synthesizer_data_dir}/sps/cloudy/{args.sps_grid}_cloudy-{args.cloudy_grid}'
+
+    # make output directories 
     Path(output_dir).mkdir(parents=True, exist_ok=True)
 
     # for submission system output files
     Path(f'{output_dir}/output').mkdir(parents=True, exist_ok=True)
 
-    return output_dir
+    # add the SPS grid parameters to grid_params
+    for axis in sps_grid.axes:
+        grid_params[axis] = sps_grid.bin_centres[axis]
 
+    # print fixed parameters
+    for k, v in fixed_params.items():
+        print(k,v)
 
-def make_cloudy_input_grid(output_dir, grid, cloudy_params):
-    """
-    Create a grid of CLOUDY input files for a given SPS grid.
+    # print grid parameters, including SPS parameters
+    for k, v in grid_params.items():
+        print(k,v)
 
-    Parameters
-    ----------
-    output_dir : str
-        Where to write the input files.
-    grid : obj
-        A synthesizer `Grid` object
-    cloudy_params : dict
-        Dictionary of cloudy parameters
-
-    Returns
-    -------
-    N : int
-        Total number of models.
-    """
-
-    ia_ref = grid.get_nearest_index(cloudy_params['log10age_ref'],
-                                    grid.log10ages)
-    iZ_ref = grid.get_nearest_index(cloudy_params['Z_ref'], grid.metallicities)
+    # get the indices of the reference grid point (this is used by the reference model)
+    sps_ref_grid_point = sps_grid.get_grid_point([cloudy_params[k+'_ref'] for k in sps_grid.axes])
 
     # add these to the parameter file
-    cloudy_params['ia_ref'] = int(ia_ref)
-    cloudy_params['iZ_ref'] = int(iZ_ref)
+    for k, i in zip(sps_grid.axes, sps_ref_grid_point):
+        cloudy_params[k+'_ref'] = i
 
-    # update the parameter file with the actual reference age and metallicity
-    # converting to float makes the resulting parameter file readable
-    cloudy_params['log10age_ref_actual'] = float(grid.log10ages[ia_ref])
-    cloudy_params['Z_ref_actual'] = float(grid.metallicities[iZ_ref])
 
-    na = len(grid.ages)
-    nZ = len(grid.metallicities)
-    n = na * nZ
+    # save all parameters
+    yaml.dump(cloudy_params, open(f'{output_dir}/params.yaml', 'w'))
 
-    for iZ in range(nZ):
+    # get properties of the grid
+    axes, n_axes, shape, n_models, mesh, model_list, index_list = get_grid_properties(grid_params, verbose = True)
 
-        # --- get metallicity
-        Z = grid.metallicities[iZ]
+    # loop over all models
+    for i, (grid_params_, grid_index_) in enumerate(zip(model_list, index_list)):
+    
+        # get a dictionary of all parameters
+        grid_params = dict(zip(axes, grid_params_))
 
-        # ---- initialise abundances object
-        abundances = Abundances(
-            Z=Z, alpha=cloudy_params['alpha'], CO=cloudy_params['CO'], d2m=cloudy_params['d2m'])
+        # get a dictionary of the parameter grid point
+        grid_index = dict(zip(axes, grid_index_))
 
-        for ia in range(na):
+        # get a dictionary of just the SPS parameters
+        sps_params = {k:grid_params[k] for k in sps_grid.axes}
 
-            if cloudy_params['U_model'] == 'ref':
+        # get a dictionary of the SPS parameter grid point
+        sps_index = {k:grid_index[k] for k in sps_grid.axes}
 
-                delta_log10Q = grid.log10Q['HI'][ia, iZ] - \
-                    grid.log10Q['HI'][ia_ref, iZ_ref]
+        # get a tuple of the SPS grid point
+        sps_grid_point = tuple(grid_index[k] for k in sps_grid.axes)
 
-                if cloudy_params['geometry'] == 'spherical':
+        # join the fixed and grid parameters 
+        cloudy_params = fixed_params | grid_params
 
-                    # SW: I'm not sure I fully understand this
-                    log10U = cloudy_params['log10U_ref'] + (1/3) * delta_log10Q
+        # set cloudy metallicity parameter to the stellar metallicity
+        if 'metallicities' in sps_grid.axes:
+            cloudy_params['Z'] = sps_params['metallicities']
+        elif 'log10metallicities' in sps_grid.axes:
+            cloudy_params['Z'] = 10**sps_params['log10metallicities']
+        
+        # create abundances object
+        abundances = Abundances(cloudy_params['Z'], d2m=cloudy_params['d2m'], alpha=cloudy_params['alpha'], N=cloudy_params['N'], C=cloudy_params['C'])
 
-                elif cloudy_params['geometry'] == 'planeparallel':
+        # if reference U model is used
+        if cloudy_params['U_model'] == 'ref':
 
-                    # log10U just scales with log10Q
-                    log10U = cloudy_params['log10U_ref'] + delta_log10Q
+            # calculate the difference between the reference log10Q (LyC continuum luminosity) and the current grid point
+            delta_log10Q = sps_grid.log10Q['HI'][sps_grid_point] - sps_grid.log10Q['HI'][sps_ref_grid_point]
 
-                else:
+            # for spherical geometry the effective log10U is this
+            if cloudy_params['geometry'] == 'spherical':
 
-                    print(f"ERROR: do not understand geometry choice: {cloudy_params['geometry']}")
+                log10U = cloudy_params['log10U_ref'] + (1/3) * delta_log10Q
 
-            elif cloudy_params['U_model'] == 'fixed':
+            # for plane-parallel geometry the effective just scales with log10Q
+            elif cloudy_params['geometry'] == 'planeparallel':
 
-                log10U = cloudy_params['log10U_ref']
+                log10U = cloudy_params['log10U_ref'] + delta_log10Q
 
             else:
 
-                print(f"ERROR: do not understand U model choice: {cloudy_params['U_model']}")
+                print(f"ERROR: do not understand geometry choice: {cloudy_params['geometry']}")
 
-            lam = grid.lam
-            lnu = grid.spectra['stellar'][ia, iZ]
+        # if fixed U model is used
+        elif cloudy_params['U_model'] == 'fixed':
 
-            model_name = f'{ia}_{iZ}'
+            log10U = cloudy_params['log10U_ref']
 
-            cloudy_params['log10U'] = float(log10U)
+        else:
 
-            # this returns the relevant shape commands, in this case for a tabulated SED
-            shape_commands = ShapeCommands.table_sed(model_name, lam, lnu, output_dir = './data/')
+            print(f"ERROR: do not understand U model choice: {cloudy_params['U_model']}")
 
-            # create cloudy input file
-            create_cloudy_input(model_name, shape_commands, abundances, output_dir = output_dir, **cloudy_params)
+        # set log10U to provide cloudy
+        cloudy_params['log10U'] = float(log10U)
 
-            # # old function left in for now for reference
-            # create_cloudy_input(model_name, lam, lnu, abundances,
-            #                     output_dir=output_dir, **cloudy_params)
+        # get wavelength
+        lam = sps_grid.lam # AA
 
-            # write filename out
-            with open(f"{output_dir}/input_names.txt", "a") as myfile:
-                myfile.write(f'{model_name}\n')
+        # get luminosity 
+        lnu = grid.spectra['stellar'][sps_grid_point]
 
-    yaml.dump(cloudy_params, open(f'{output_dir}/params.yaml', 'w'))
+        # this returns the relevant shape commands, in this case for a tabulated SED
+        shape_commands = ShapeCommands.table_sed(str(i), lam, lnu,  output_dir=output_dir)
 
-    return n
+        # create cloudy input file
+        create_cloudy_input(str(i), shape_commands, abundances, output_dir = output_dir, **cloudy_params)
 
-
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(description=('Write cloudy input files '
-                                                  'and submission script for '
-                                                  'a given SPS grid.'))
-
-    parser.add_argument("-dir", "--directory", type=str, required=False, default=None)
-
-    parser.add_argument("-m", "--machine", type=str, choices=['cosma7', 'apollo'], default=None, help=(
-        'Write a submission script for the ', 'specified machine. Default is None - ', 'write no submission script.'))
-
-    parser.add_argument("-sps", "--sps_grid", type=str,
-                        nargs='+', required=False, default=None,
-                        help=('The SPS grid(s) to run the cloudy grid on. '
-                              'Multiple grids can be listed as: \n '
-                              '  --sps_grid grid_1 grid_2'))
-
-    parser.add_argument("-p", "--params", type=str, required=False, default='default.yaml',
-                        help='YAML parameter file of cloudy parameters')
-
-    parser.add_argument("-c", "--cloudy", type=str, nargs='?',
-                        default='$CLOUDY17', help='CLOUDY executable call')
-
-    args = parser.parse_args()
-
-    synthesizer_data_dir = args.directory
-    cloudy = args.cloudy
-
-    print(f"Loading the cloudy parameters from: {args.params}")
-
-    # load the cloudy parameters you are going to run
-    c_params, c_name = load_cloudy_parameters(args.params)
-
-    for i, cloudy_name in enumerate(c_name):
-        print(i, cloudy_name)
-
-    if args.sps_grid:
-
-        for sps_grid in args.sps_grid:
-
-            print(f"Loading the SPS grid: {sps_grid}")
-
-            # load the specified SPS grid
-            grid = Grid(sps_grid, grid_dir=f'{synthesizer_data_dir}/grids')
-
-            for i, (cloudy_params, cloudy_name) in \
-                    enumerate(zip(c_params, c_name)):
-
-                output_dir = make_directories(synthesizer_data_dir, sps_grid,
-                                              cloudy_name)
-
-                print((f"Generating cloudy grid for ({i}) "
-                       f"{cloudy_name} in {output_dir}"))
-
-                N = make_cloudy_input_grid(output_dir, grid, cloudy_params)
-
-                if args.machine == 'apollo':
-                    apollo_submission_script(N, output_dir, cloudy)
-                elif args.machine == 'cosma7':
-                    cosma7_submission_script(N, output_dir, cloudy,
-                                             cosma_project='cosma7',
-                                             cosma_account='dp004')
-                elif args.machine is None:
-                    print(("No machine specified. Skipping "
-                           "submission script write out"))
-                else:
-                    ValueError(f'Machine {args.machine} not recognised.')
+    # create submission script
+    if args.machine == 'apollo':
+        apollo_submission_script(n_models, output_dir, args.cloudy_path, cloudy_params['cloudy_version'])
