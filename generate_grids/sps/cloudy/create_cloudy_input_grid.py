2395fb22528205e963e1ed7f0b7779413d98e005
Stephen Wilkins <sw376@mps026505.mps.schools.susx.ac.uk>
2023-06-21 11:56:30 +0100

    adapted to create output HDF5 file as well

--- generate_grids/sps/cloudy/create_cloudy_input_grid.py
+++ generate_grids/sps/cloudy/create_cloudy_input_grid.py
@@ -1,5 +1,6 @@
 """
 Create a grid of cloudy models
+Also create an output HDF5 for ultimately containing the grid
 """
 
 import numpy as np
@@ -77,6 +78,7 @@ if __name__ == "__main__":
     sps_grid_name = args.sps_grid
     sps_grid = Grid(sps_grid_name, grid_dir=f'{args.synthesizer_data_dir}/grids')
 
+
     # get name of new grid (concatenation of sps_grid and cloudy parameter file)
     new_grid_name = f'{args.sps_grid}_cloudy-{args.cloudy_grid}'
 
@@ -89,9 +91,14 @@ if __name__ == "__main__":
     # for submission system output files
     Path(f'{output_dir}/output').mkdir(parents=True, exist_ok=True)
 
+    # set a list of the axes
+    axes = list(sps_grid.axes) + list(grid_params.keys())
+    print('axes:', axes)
+
+
     # add the SPS grid parameters to grid_params
     for axis in sps_grid.axes:
-        grid_params[axis] = sps_grid.bin_centres[axis]
+        grid_params[axis] = getattr(sps_grid, axis)
 
     # print fixed parameters
     for k, v in fixed_params.items():
@@ -101,12 +108,15 @@ if __name__ == "__main__":
     for k, v in grid_params.items():
         print(k,v)
 
-    # get the indices of the reference grid point (this is used by the reference model)
-    sps_ref_grid_point = sps_grid.get_grid_point([fixed_params[k+'_ref'] for k in sps_grid.axes])
+    # if the U model is the reference model (i.e. not fixed) save the grid point for the reference values
+    if fixed_params['U_model'] == 'ref':
 
-    # add these to the parameter file
-    for k, i in zip(sps_grid.axes, sps_ref_grid_point):
-        fixed_params[k+'_ref_index'] = i
+        # get the indices of the reference grid point (this is used by the reference model)
+        sps_ref_grid_point = sps_grid.get_grid_point([fixed_params[k+'_ref'] for k in sps_grid.axes])
+
+        # add these to the parameter file
+        for k, i in zip(sps_grid.axes, sps_ref_grid_point):
+            fixed_params[k+'_ref_index'] = i
 
     # combine all parameters
     params = fixed_params | grid_params
@@ -115,7 +125,7 @@ if __name__ == "__main__":
     yaml.dump(params, open(f'{output_dir}/params.yaml', 'w'))
 
     # get properties of the grid
-    axes, n_axes, shape, n_models, mesh, model_list, index_list = get_grid_properties(grid_params, verbose = True)
+    n_axes, shape, n_models, mesh, model_list, index_list = get_grid_properties(axes, grid_params, verbose = True)
 
     
     # create new synthesizer grid to contain the new grid    
@@ -126,118 +136,112 @@ if __name__ == "__main__":
         # open the original SPS model grid
         with h5py.File(f'{args.synthesizer_data_dir}/grids/{sps_grid_name}.hdf5', 'r') as hf_sps:
 
+            hf_sps.visit(print)
+
             # copy top-level attributes
             for k, v in hf_sps.attrs.items():
-                print('----- SPS attributes')
-                print(k, v)
                 hf.attrs[k] = v
 
             # add attribute with the original SPS grid axes 
-            hf.attrs['sps_axes'] = hf_sps.attrs['grid_axes']
+            hf.attrs['sps_axes'] = hf_sps.attrs['axes']
 
-            # add in log10Q 
-            hf['log10Q'] = hf_sps['log10Q']
+            # copy log10Q over
+            hf_sps.copy('log10Q', hf) 
 
         # add attribute with full grid axes
-        hf.attrs['grid_axes'] = list(grid_params.keys())
+        hf.attrs['axes'] = axes
 
         # add the bin centres for the grid bins
-        for axis in hf.attrs['grid_axes']:
-            hf['bin_centres'][axis] = grid_params[axis]
+        for axis in axes:
+            hf[f'axes/{axis}'] = grid_params[axis]
 
         # add other parameters as attributes
         for k,v in params.items():
-            print('----- cloudy attributes')
-            print(k,v)
             hf.attrs['k'] = v
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # # loop over all models
-    # for i, (grid_params_tuple, grid_index_tuple) in enumerate(zip(model_list, index_list)):
-    
-    #     # get a dictionary of all parameters
-    #     grid_params_ = dict(zip(axes, grid_params_tuple))
-
-    #     # get a dictionary of the parameter grid point
-    #     grid_index_ = dict(zip(axes, grid_index_tuple))
-
-    #     # get a dictionary of just the SPS parameters
-    #     sps_params_ = {k:grid_params_[k] for k in sps_grid.axes}
-
-    #     # get a dictionary of the SPS parameter grid point
-    #     sps_index_ = {k:grid_index_[k] for k in sps_grid.axes}
-
-    #     # get a tuple of the SPS grid point
-    #     sps_grid_point = tuple(grid_index_[k] for k in sps_grid.axes)
-
-    #     # join the fixed and current iteration of the grid parameters 
-    #     params_ = fixed_params | grid_params_
-
-    #     # set cloudy metallicity parameter to the stellar metallicity
-    #     if 'metallicities' in sps_grid.axes:
-    #         params_['Z'] = sps_params_['metallicities']
-    #     elif 'log10metallicities' in sps_grid.axes:
-    #         params_['Z'] = 10**sps_params_['log10metallicities']
+        print('-'*50)
+        print('---- attributes')
+        for k,v in hf.attrs.items():
+            print(k,v)
+        print('---- groups and datasets')
+        hf.visit(print)
         
-    #     # create abundances object
-    #     abundances = Abundances(params_['Z'], d2m=params_['d2m'], alpha=params_['alpha'], N=params_['N'], C=params_['C'])
 
-    #     # if reference U model is used
-    #     if params_['U_model'] == 'ref':
 
-    #         # calculate the difference between the reference log10Q (LyC continuum luminosity) and the current grid point
-    #         delta_log10Q = sps_grid.log10Q['HI'][sps_grid_point] - sps_grid.log10Q['HI'][sps_ref_grid_point]
+    # loop over all models
+    for i, (grid_params_tuple, grid_index_tuple) in enumerate(zip(model_list, index_list)):
+    
+        # get a dictionary of all parameters
+        grid_params_ = dict(zip(axes, grid_params_tuple))
 
-    #         # for spherical geometry the effective log10U is this
-    #         if params_['geometry'] == 'spherical':
+        # get a dictionary of the parameter grid point
+        grid_index_ = dict(zip(axes, grid_index_tuple))
 
-    #             log10U = params_['log10U_ref'] + (1/3) * delta_log10Q
+        # get a dictionary of just the SPS parameters
+        sps_params_ = {k:grid_params_[k] for k in sps_grid.axes}
 
-    #         # for plane-parallel geometry the effective just scales with log10Q
-    #         elif params_['geometry'] == 'planeparallel':
+        # get a dictionary of the SPS parameter grid point
+        sps_index_ = {k:grid_index_[k] for k in sps_grid.axes}
 
-    #             log10U = params_['log10U_ref'] + delta_log10Q
+        # get a tuple of the SPS grid point
+        sps_grid_point = tuple(grid_index_[k] for k in sps_grid.axes)
 
-    #         else:
+        # join the fixed and current iteration of the grid parameters 
+        params_ = fixed_params | grid_params_
 
-    #             print(f"ERROR: do not understand geometry choice: {params_['geometry']}")
+        # set cloudy metallicity parameter to the stellar metallicity
+        if 'metallicities' in sps_grid.axes:
+            params_['Z'] = sps_params_['metallicities']
+        elif 'log10metallicities' in sps_grid.axes:
+            params_['Z'] = 10**sps_params_['log10metallicities']
+        
+        # create abundances object
+        abundances = Abundances(params_['Z'], d2m=params_['d2m'], alpha=params_['alpha'], N=params_['N'], C=params_['C'])
 
-    #     # if fixed U model is used
-    #     elif params_['U_model'] == 'fixed':
+        # if reference U model is used
+        if params_['U_model'] == 'ref':
 
-    #         log10U = params_['log10U_ref']
+            # calculate the difference between the reference log10Q (LyC continuum luminosity) and the current grid point
+            delta_log10Q = sps_grid.log10Q['HI'][sps_grid_point] - sps_grid.log10Q['HI'][sps_ref_grid_point]
 
-    #     else:
+            # for spherical geometry the effective log10U is this
+            if params_['geometry'] == 'spherical':
 
-    #         print(f"ERROR: do not understand U model choice: {params_['U_model']}")
+                log10U = params_['log10U_ref'] + (1/3) * delta_log10Q
 
-    #     # set log10U to provide cloudy
-    #     params_['log10U'] = float(log10U)
+            # for plane-parallel geometry the effective just scales with log10Q
+            elif params_['geometry'] == 'planeparallel':
 
-    #     # get wavelength
-    #     lam = sps_grid.lam # AA
+                log10U = params_['log10U_ref'] + delta_log10Q
 
-    #     # get luminosity 
-    #     lnu = sps_grid.spectra['stellar'][sps_grid_point]
+            else:
 
-    #     # this returns the relevant shape commands, in this case for a tabulated SED
-    #     shape_commands = ShapeCommands.table_sed(str(i+1), lam, lnu,  output_dir=output_dir)
+                print(f"ERROR: do not understand geometry choice: {params_['geometry']}")
 
-    #     # create cloudy input file
-    #     create_cloudy_input(str(i+1), shape_commands, abundances, output_dir = output_dir, **params_)
+        # if fixed U model is used
+        elif params_['U_model'] == 'fixed':
 
-    # # create submission script
-    # if args.machine == 'apollo':
-    #     apollo_submission_script(n_models, output_dir, args.cloudy_path, params_['cloudy_version'])
+            log10U = params_['log10U_ref']
+
+        else:
+
+            print(f"ERROR: do not understand U model choice: {params_['U_model']}")
+
+        # set log10U to provide cloudy
+        params_['log10U'] = float(log10U)
+
+        # get wavelength
+        lam = sps_grid.lam # AA
+
+        # get luminosity 
+        lnu = sps_grid.spectra['stellar'][sps_grid_point]
+
+        # this returns the relevant shape commands, in this case for a tabulated SED
+        shape_commands = ShapeCommands.table_sed(str(i+1), lam, lnu,  output_dir=output_dir)
+
+        # create cloudy input file
+        create_cloudy_input(str(i+1), shape_commands, abundances, output_dir = output_dir, **params_)
+
+    # create submission script
+    if args.machine == 'apollo':
+        apollo_submission_script(n_models, output_dir, args.cloudy_path, params_['cloudy_version'])
