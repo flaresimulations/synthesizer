79f338768e2706b85fd4a3ea60bcead27646d0f2
Stephen Wilkins <sw376@mps026505.mps.schools.susx.ac.uk>
2023-06-21 14:05:34 +0100

    bugfix

--- generate_grids/sps/cloudy/create_synthesizer_grid.py
+++ generate_grids/sps/cloudy/create_synthesizer_grid.py
@@ -163,7 +163,7 @@ def add_spectra(grid_name, synthesizer_data_dir):
             # calcualte normalisation
             normalisation = hf['log10Q/HI'][indices] - np.log10(Q)
 
-            print(i, normalisation, len(spectra['incident'][indices]))
+            print(i, normalisation, np.log10(Q), hf['log10Q/HI'][indices])
 
             # save normalisation for later use (rescaling lines)
             spectra['normalisation'][indices] = normalisation
