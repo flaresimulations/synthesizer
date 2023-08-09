dec67c60b6d9967e60306fbed70e70e8ac0caf2f
Stephen Wilkins <sw376@mps026505.mps.schools.susx.ac.uk>
2023-08-09 14:27:00 +0100

    deleted old shell scripts

--- generate_grids/sps/cloudy/create_synthesizer_grid.sh
+++
@@ -1,6 +0,0 @@
-
-GRID="bpass-2.2.1-bin_chabrier03-0.1,300.0_cloudy-c17.03-fixedU-log10U"
-SYNTHESIZER_DATA_DIR="/research/astrodata/highz/synthesizer/"
-INCLUDE_SPECTRA=True
-
-python create_synthesizer_grid.py -synthesizer_data_dir $SYNTHESIZER_DATA_DIR -grid $GRID -include_spectra $INCLUDE_SPECTRA
